4e9ee30cf595d5a9bb5f5273ac7debc898a9bef6
vinceallenvince <vince@vinceallen.com>
2014-08-23 20:16:09 +0000

    Deployed to Github Pages

---
+++ scripts/colorpalette.js
@@ -0,0 +1,338 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.ColorPalette=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+/*jshint supernew:true */
+/** @namespace */
+var Utils = {
+  name: 'Utils'
+};
+
+/**
+ * Extends the properties and methods of a superClass onto a subClass.
+ *
+ * @function extend
+ * @memberof Utils
+ * @param {Object} subClass The subClass.
+ * @param {Object} superClass The superClass.
+ */
+Utils.extend = function(subClass, superClass) {
+  function F() {}
+  F.prototype = superClass.prototype;
+  subClass.prototype = new F;
+  subClass.prototype.constructor = subClass;
+  subClass._superClass = superClass.prototype;
+};
+
+/**
+ * Generates a psuedo-random number within a range.
+ *
+ * @function getRandomNumber
+ * @memberof Utils
+ * @param {number} low The low end of the range.
+ * @param {number} high The high end of the range.
+ * @param {boolean} [flt] Set to true to return a float.
+ * @returns {number} A number.
+ */
+Utils.getRandomNumber = function(low, high, flt) {
+  if (flt) {
+    return Math.random()*(high-(low-1)) + low;
+  }
+  return Math.floor(Math.random()*(high-(low-1))) + low;
+};
+
+/**
+ * Re-maps a number from one range to another.
+ *
+ * @function map
+ * @memberof Utils
+ * @param {number} value The value to be converted.
+ * @param {number} min1 Lower bound of the value's current range.
+ * @param {number} max1 Upper bound of the value's current range.
+ * @param {number} min2 Lower bound of the value's target range.
+ * @param {number} max2 Upper bound of the value's target range.
+ * @returns {number} A number.
+ */
+Utils.map = function(value, min1, max1, min2, max2) { // returns a new value relative to a new range
+  var unitratio = (value - min1) / (max1 - min1);
+  return (unitratio * (max2 - min2)) + min2;
+};
+
+/**
+ * Adds an event listener to a DOM element.
+ *
+ * @function _addEvent
+ * @memberof System
+ * @private
+ * @param {Object} target The element to receive the event listener.
+ * @param {string} eventType The event type.
+ * @param {function} The function to run when the event is triggered.
+ */
+Utils.addEvent = function(target, eventType, handler) {
+  if (target.addEventListener) { // W3C
+    target.addEventListener(eventType, handler, false);
+  } else if (target.attachEvent) { // IE
+    target.attachEvent('on' + eventType, handler);
+  }
+};
+
+/**
+ * Converts degrees to radians.
+ *
+ * @function degreesToRadians
+ * @memberof Utils
+ * @param {number} degrees The degrees value to be converted.
+ * @returns {number} A number in radians.
+ */
+Utils.degreesToRadians = function(degrees) {
+  if (typeof degrees !== 'undefined') {
+    return 2 * Math.PI * (degrees/360);
+  } else {
+    if (typeof console !== 'undefined') {
+      throw new Error('Error: Utils.degreesToRadians is missing degrees param.');
+    }
+  }
+};
+
+/**
+ * Converts radians to degrees.
+ *
+ * @function radiansToDegrees
+ * @memberof Utils
+ * @param {number} radians The radians value to be converted.
+ * @returns {number} A number in degrees.
+ */
+Utils.radiansToDegrees = function(radians) {
+  if (typeof radians !== 'undefined') {
+    return radians * (180/Math.PI);
+  } else {
+    if (typeof console !== 'undefined') {
+      throw new Error('Error: Utils.radiansToDegrees is missing radians param.');
+    }
+  }
+};
+
+/**
+ * Constrain a value within a range.
+ *
+ * @function constrain
+ * @memberof Utils
+ * @param {number} val The value to constrain.
+ * @param {number} low The lower bound of the range.
+ * @param {number} high The upper bound of the range.
+ * @returns {number} A number.
+ */
+Utils.constrain = function(val, low, high) {
+  if (val > high) {
+    return high;
+  } else if (val < low) {
+    return low;
+  }
+  return val;
+};
+
+/**
+ * Determines if one object is inside another.
+ *
+ * @function isInside
+ * @memberof Utils
+ * @param {Object} obj The object.
+ * @param {Object} container The containing object.
+ * @returns {boolean} Returns true if the object is inside the container.
+ */
+Utils.isInside = function(obj, container) {
+  if (!obj || !container) {
+    throw new Error('isInside() requires both an object and a container.');
+  }
+
+  obj.width = obj.width || 0;
+  obj.height = obj.height || 0;
+  container.width = container.width || 0;
+  container.height = container.height || 0;
+
+  if (obj.location.x + obj.width / 2 > container.location.x - container.width / 2 &&
+    obj.location.x - obj.width / 2 < container.location.x + container.width / 2 &&
+    obj.location.y + obj.height / 2 > container.location.y - container.height / 2 &&
+    obj.location.y - obj.height / 2 < container.location.y + container.height / 2) {
+    return true;
+  }
+  return false;
+};
+
+/**
+ * Capitalizes the first character in a string.
+ *
+ * @function capitalizeFirstLetter
+ * @memberof Utils
+ * @param {string} string The string to capitalize.
+ * @returns {string} The string with the first character capitalized.
+ */
+Utils.capitalizeFirstLetter = function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+module.exports = Utils;
+},{}],2:[function(_dereq_,module,exports){
+var Utils = _dereq_('drawing-utils-lib');
+/**
+ * Creates a new ColorPalette object.
+ *
+ * Use this class to create a palette of colors randomly selected
+ * from a range created with initial start and end colors. You
+ * can also generate gradients that smoothly interpolate from
+ * start and end colors.
+ *
+ * @param {string|number} [opt_id=] An optional id. If an id is not passed, a default id is created.
+ * @constructor
+ */
+function ColorPalette(opt_id) {
+
+  /**
+   * Holds a list of arrays representing 3-digit color values
+   * smoothly interpolated between start and end colors.
+   * @private
+   */
+  this._gradients = [];
+
+  /**
+   * Holds a list of arrays representing 3-digit color values
+   * randomly selected from start and end colors.
+   * @private
+   */
+  this._colors = [];
+
+  this.id = opt_id || ColorPalette._idCount;
+  ColorPalette._idCount++; // increment id
+}
+
+/**
+ * Increments as each ColorPalette is created.
+ * @type number
+ * @default 0
+ * @private
+ */
+ColorPalette._idCount = 0;
+
+ColorPalette.prototype.name = 'ColorPalette';
+
+/**
+ * Creates a color range of 255 colors from the passed start and end colors.
+ * Adds a random selection of these colors to the color property of
+ * the color palette.
+ *
+ * @param {Object} options A set of required options
+ *    that includes:
+ *    options.min {number} The minimum number of colors to add.
+ *    options.max {number} The maximum number of color to add.
+ *    options.startColor {Array} The beginning color of the color range.
+ *    options.endColor {Array} The end color of the color range.
+ */
+ColorPalette.prototype.addColor = function(options) {
+
+  if (!options.min || !options.max || !options.startColor || !options.endColor) {
+    throw new Error('ColorPalette.addColor must pass min, max, startColor and endColor options.');
+  }
+
+  var i, ln, colors;
+
+  ln = Utils.getRandomNumber(options.min, options.max);
+  colors = ColorPalette._createColorRange(options.startColor, options.endColor, 255);
+
+  for (i = 0; i < ln; i++) {
+    this._colors.push(colors[Utils.getRandomNumber(0, colors.length - 1)]);
+  }
+
+  return this;
+};
+
+/**
+ * Creates an array of RGB color values interpolated between
+ * a passed startColor and endColor.
+ *
+ * @param {Array} startColor The beginning of the color array.
+ * @param {Array} startColor The end of the color array.
+ * @param {number} totalColors The total numnber of colors to create.
+ * @returns {Array} An array of color values.
+ */
+ColorPalette._createColorRange = function(startColor, endColor, totalColors) {
+
+  var i, colors = [],
+      startRed = startColor[0],
+      startGreen = startColor[1],
+      startBlue = startColor[2],
+      endRed = endColor[0],
+      endGreen = endColor[1],
+      endBlue = endColor[2],
+      diffRed, diffGreen, diffBlue,
+      newRed, newGreen, newBlue;
+
+  diffRed = endRed - startRed;
+  diffGreen = endGreen - startGreen;
+  diffBlue = endBlue - startBlue;
+
+  for (i = 0; i < totalColors; i++) {
+    newRed = parseInt(diffRed * i/totalColors, 10) + startRed;
+    newGreen = parseInt(diffGreen * i/totalColors, 10) + startGreen;
+    newBlue = parseInt(diffBlue * i/totalColors, 10) + startBlue;
+    colors.push([newRed, newGreen, newBlue]);
+  }
+  return colors;
+};
+
+/**
+ * @returns An array representing a randomly selected color
+ *    from the colors property.
+ * @throws {Error} If the colors property is empty.
+ */
+ColorPalette.prototype.getColor = function() {
+
+  if (this._colors.length > 0) {
+    return this._colors[Utils.getRandomNumber(0, this._colors.length - 1)];
+  } else {
+    throw new Error('ColorPalette.getColor: You must add colors via addColor() before using getColor().');
+  }
+};
+
+// TODO: add the following
+
+/**
+ * Adds color arrays representing a color range to the gradients property.
+ *
+ * @param {Object} options A set of required options
+ *    that includes:
+ *    options.startColor {Array} The beginning color of the color range.
+ *    options.endColor {Array} The end color of the color range.
+ *    options.totalColors {number} The total number of colors in the gradient.
+ * @private
+ */
+/*ColorPalette.prototype.createGradient = function(options) {
+
+  if (!options.startColor || !options.endColor || !options.totalColors) {
+    throw new Error('ColorPalette.addColor must pass startColor, endColor and totalColors options.');
+  }
+  this.startColor = options.startColor;
+  this.endColor = options.endColor;
+  this.totalColors = options.totalColors || 255;
+  this._gradients.push(ColorPalette._createColorRange(this.startColor, this.endColor, this.totalColors));
+};*/
+
+/**
+ * Renders a strip of colors representing the color range
+ * in the colors property.
+ *
+ * @param {Object} parent A DOM object to contain the color strip.
+ */
+/*ColorPalette.prototype.createSampleStrip = function(parent) {
+
+  var i, max, div;
+
+  for (i = 0, max = this._colors.length; i < max; i++) {
+    div = document.createElement('div');
+    div.className = 'color-sample-strip';
+    div.style.background = 'rgb(' + this._colors[i].toString() + ')';
+    parent.appendChild(div);
+  }
+};*/
+
+module.exports = ColorPalette;
+
+},{"drawing-utils-lib":1}]},{},[2])
+(2)
+});
